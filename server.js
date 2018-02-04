d088bd951b12ef51c907532243cdbe599405baa9
Graygzou <gregoire.boiron@gmail.com>
2018-02-03 19:52:14 -0500

    Create basic server file + tidy up the project

---
+++ server.js
@@ -0,0 +1,53 @@
+/**
+ * @author : Grégoire Boiron <gregoire.boiron@gmail.com>
+ * @version : 0.1.0
+ */
+
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+// Initialize app object.
+var app = express();
+
+// Set path to views.
+//app.set('views', './views');
+
+// Let express know there's a public directory.
+app.use(express.static('./public'));
+
+// -------------------------------------
+// Basic Routing
+// -------------------------------------
+
+// Index page
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+	fs.readFile('./views/index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+app.get('/Exercice1', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+app.get('/Exercice2', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    fs.readFile('../index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(8080);
