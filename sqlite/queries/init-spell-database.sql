aa607ca54454ebd15f32c792ffda381009732c47
Graygzou <gregoire.boiron@gmail.com>
2018-02-20 15:42:01 -0500

    Can add spells to the sqlite database

--- sqlite/queries/init-spell-database.sql
+++ sqlite/queries/init-spell-database.sql
@@ -2,22 +2,30 @@
 -- Create basic tables
 -------------------------------------
 
+-- Reset the database schema
+DROP TABLE IF EXISTS UserClass;
+DROP TABLE IF EXISTS Spell;
+DROP TABLE IF EXISTS School;
+DROP TABLE IF EXISTS Ingredient;
+DROP TABLE IF EXISTS Invoque;				
+DROP TABLE IF EXISTS Need;
+
 -- Class table (Cleric, Wizard, ...)
 CREATE TABLE IF NOT EXISTS UserClass (
-	class_id INT PRIMARY KEY,
+	class_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
 	description TEXT
 );
 
 -- Spell table
 CREATE TABLE IF NOT EXISTS Spell (
-	spell_id INT PRIMARY KEY,
-	school_id INT,
+	spell_id INTEGER PRIMARY KEY,
+	school_id INTEGER,
 	name TEXT, 
 	description TEXT, 
-	provideResistance INT,
-	isVerbose INT ,
-	isSomatic INT,
+	provideResistance INTEGER,
+	isVerbose INTEGER,
+	isSomatic INTEGER,
 	CHECK (provideResistance IN (0, 1)),
 	CHECK (isVerbose IN (0, 1)),
 	CHECK (isSomatic IN (0, 1)),
@@ -25,13 +33,13 @@ CREATE TABLE IF NOT EXISTS Spell (
 );
 
 CREATE TABLE IF NOT EXISTS School (
-	school_id INT PRIMARY KEY,
+	school_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL, 
 	description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Ingredient (
-	ingredient_id INT PRIMARY KEY,
+	ingredient_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
 	description TEXT
 );
@@ -39,8 +47,8 @@ CREATE TABLE IF NOT EXISTS Ingredient (
 -- Create "joining tables"
 CREATE TABLE IF NOT EXISTS Invoque (
 	class_id TEXT,
-	spell_id INT,
-	spellLevel INT,
+	spell_id INTEGER,
+	spellLevel INTEGER,
 	FOREIGN KEY (class_id) REFERENCES UserClass (class_id),
 	FOREIGN KEY (spell_id) REFERENCES Spell (spell_id),
 	CHECK (spellLevel >= 0 AND spellLevel < 10),
@@ -49,7 +57,7 @@ CREATE TABLE IF NOT EXISTS Invoque (
 					
 CREATE TABLE IF NOT EXISTS Need (
 	indregient_type TEXT,
-	cost INT,
+	cost INTEGER,
 	ingredient_id TEXT,
 	spell_id TEXT,
 	FOREIGN KEY (ingredient_id) REFERENCES Ingredients (ingredient_id),
